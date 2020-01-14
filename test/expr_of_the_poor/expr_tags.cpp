42a4e02670b268ab4ba246f1d0f868bdf842d821
Picaud Vincent <picaud.vincent@gmail.com>
2020-01-14 13:06:07 +0100

    create expr_tags files

---
+++ test/expr_of_the_poor/expr_tags.cpp
@@ -0,0 +1,15 @@
+#include "LinearAlgebra/expr_of_the_poor/expr_tags.hpp"
+
+#include <gtest/gtest.h>
+
+using namespace LinearAlgebra;
+
+TEST(Expr_Tags, matrix_op)
+{
+  EXPECT_EQ(_identity_, conjugate(_conjugate_));
+  EXPECT_EQ(_transConj_, transpose(_conjugate_));
+  EXPECT_EQ(_transConj_, conjugate(_transpose_));
+
+  EXPECT_TRUE(does_it_transpose_matrix_dimension(_transpose_));
+  EXPECT_FALSE(does_it_transpose_matrix_dimension(_conjugate_));
+}

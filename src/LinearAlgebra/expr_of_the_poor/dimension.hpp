97e8f67bb24fb3301485852a75ea828db19d6ee9
Picaud Vincent <picaud.vincent@gmail.com>
2020-03-15 10:11:17 +0100

    predicate

--- src/LinearAlgebra/expr_of_the_poor/dimension.hpp
+++ src/LinearAlgebra/expr_of_the_poor/dimension.hpp
@@ -8,7 +8,7 @@
 namespace LinearAlgebra
 {
   template <typename I_SIZE, typename J_SIZE>
-  class Matrix_Dimension
+  class Matrix_Dimension_Predicate
   {
     static_assert(Is_Std_Integral_Constant_Size_Or_Std_Size_v<I_SIZE> and
                   Is_Std_Integral_Constant_Size_Or_Std_Size_v<J_SIZE>);
@@ -20,27 +20,38 @@ namespace LinearAlgebra
    private:
     I_size_type _I_size;
     J_size_type _J_size;
+    bool _is_valid;
 
    public:
-    Matrix_Dimension(I_size_type I_size, J_size_type J_size) : _I_size{I_size}, _J_size{J_size} {}
+    Matrix_Dimension_Predicate() noexcept : _I_size{}, _J_size{}, _is_valid{false} {}
+    Matrix_Dimension_Predicate(const I_size_type I_size, const J_size_type J_size) noexcept
+        : _I_size{I_size}, _J_size{J_size}, _is_valid{true}
+    {
+    }
+
+    operator bool() const noexcept { return _is_valid; }
 
     I_size_type
     I_size() const noexcept
     {
+      assert(*this);
       return _I_size;
-    };
+    }
+
     J_size_type
     J_size() const noexcept
     {
+      assert(*this);
       return _J_size;
-    };
+    }
   };
 
   template <typename IMPL>
-  Matrix_Dimension<typename IMPL::I_size_type, typename IMPL::J_size_type>
-  dimension(const Matrix_Crtp<IMPL>& M)
+  Matrix_Dimension_Predicate<typename IMPL::I_size_type, typename IMPL::J_size_type>
+  dimension_predicate(const Matrix_Crtp<IMPL>& M)
   {
-    using return_type = decltype(dimension(M));
+    using return_type = decltype(dimension_predicate(M));
     return return_type{M.I_size(), M.J_size()};
   }
+
 }
--- test/expr_of_the_poor/dimension.cpp
+++ test/expr_of_the_poor/dimension.cpp
@@ -9,7 +9,7 @@ using namespace LinearAlgebra;
 TEST(Dimension, dimension)
 {
   Tiny_Matrix<int, 3, 2> M;
-  auto M_dim = dimension(M);
+  auto M_dim = dimension_predicate(M);
 
   EXPECT_EQ(M_dim.I_size(), 3);
   EXPECT_EQ(M_dim.J_size(), 2);

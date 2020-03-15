cd9764cac6cc5a930fbd279b7ced96cea98bcc71
Picaud Vincent <picaud.vincent@gmail.com>
2020-03-15 15:08:47 +0100

    comment out useless stuff

--- src/LinearAlgebra/dense/vector_crtp_size_utils.hpp
+++ src/LinearAlgebra/dense/vector_crtp_size_utils.hpp
@@ -1,49 +1,49 @@
-#pragma once
+// #pragma once
 
-#include "LinearAlgebra/dense/vector_crtp_fwd.hpp"
-#include "LinearAlgebra/meta/size_utils.hpp"
+// #include "LinearAlgebra/dense/vector_crtp_fwd.hpp"
+// #include "LinearAlgebra/meta/size_utils.hpp"
 
-namespace LinearAlgebra
-{
-  //////////////
-  // get_size //
-  //////////////
-  //
-  namespace Detail
-  {
-    template <typename IMPL, typename... IMPLs>
-    inline constexpr auto
-    get_static_size(const Vector_Crtp<IMPL>& V, const Vector_Crtp<IMPLs>&... Vs) noexcept
-    {
-      static_assert(Any_Has_Static_Size_v<IMPL, IMPLs...>);
+// namespace LinearAlgebra
+// {
+//   //////////////
+//   // get_size //
+//   //////////////
+//   //
+//   namespace Detail
+//   {
+//     template <typename IMPL, typename... IMPLs>
+//     inline constexpr auto
+//     get_static_size(const Vector_Crtp<IMPL>& V, const Vector_Crtp<IMPLs>&... Vs) noexcept
+//     {
+//       static_assert(Any_Has_Static_Size_v<IMPL, IMPLs...>);
 
-      if constexpr (Has_Static_Size_v<IMPL>)
-      {
-        return V.size();
-      }
-      else
-      {
-        return get_static_size(Vs...);
-      }
-    }
-  }
+//       if constexpr (Has_Static_Size_v<IMPL>)
+//       {
+//         return V.size();
+//       }
+//       else
+//       {
+//         return get_static_size(Vs...);
+//       }
+//     }
+//   }
 
-  // Returns vector size, a static one if any
-  //
-  // CAVEAT: we do *not* check that these sizes are identical
-  //
-  //
-  template <typename IMPL, typename... IMPLs>
-  inline constexpr auto
-  get_size(const Vector_Crtp<IMPL>& V, const Vector_Crtp<IMPLs>&... Vs) noexcept
-  {
-    if constexpr (Any_Has_Static_Size_v<IMPL, IMPLs...>)
-    {
-      return Detail::get_static_size(V, Vs...);
-    }
-    else
-    {
-      return V.size();
-    }
-  }
-}
+//   // Returns vector size, a static one if any
+//   //
+//   // CAVEAT: we do *not* check that these sizes are identical
+//   //
+//   //
+//   template <typename IMPL, typename... IMPLs>
+//   inline constexpr auto
+//   get_size(const Vector_Crtp<IMPL>& V, const Vector_Crtp<IMPLs>&... Vs) noexcept
+//   {
+//     if constexpr (Any_Has_Static_Size_v<IMPL, IMPLs...>)
+//     {
+//       return Detail::get_static_size(V, Vs...);
+//     }
+//     else
+//     {
+//       return V.size();
+//     }
+//   }
+// }
--- test/dense/vector_crtp_size_utils.cpp
+++ test/dense/vector_crtp_size_utils.cpp
@@ -7,19 +7,19 @@ using namespace LinearAlgebra;
 
 TEST(Vector_Crtp_Size_Utils, get_size)
 {
-  Tiny_Vector<int, 4> tv;
-  Vector<float> v(5);
+  // Tiny_Vector<int, 4> tv;
+  // Vector<float> v(5);
 
-  EXPECT_EQ(get_size(tv, v), 4);
-  EXPECT_EQ(get_size(tv, tv), 4);
-  EXPECT_EQ(get_size(v, v), 5);
+  // EXPECT_EQ(get_size(tv, v), 4);
+  // EXPECT_EQ(get_size(tv, tv), 4);
+  // EXPECT_EQ(get_size(v, v), 5);
 
-  auto tv_size = get_size(v, v, v, tv, tv, v, v);
+  // auto tv_size = get_size(v, v, v, tv, tv, v, v);
 
-  EXPECT_TRUE((std::is_same_v<std::integral_constant<std::size_t, 4>, decltype(tv_size)>));
+  // EXPECT_TRUE((std::is_same_v<std::integral_constant<std::size_t, 4>, decltype(tv_size)>));
 
-  auto v_size = get_size(v, v, v, v, v, v, v); 
+  // auto v_size = get_size(v, v, v, v, v, v, v); 
 
-  EXPECT_TRUE((std::is_same_v<std::size_t, decltype(v_size)>));
-  EXPECT_EQ(v_size, 5);
+  // EXPECT_TRUE((std::is_same_v<std::size_t, decltype(v_size)>));
+  // EXPECT_EQ(v_size, 5);
 }

fc4748f85dbe4c8ce36d60f62b5f3633babe0c33
picaud <picaud@am-080027980deb.intern>
2020-03-18 10:05:06 +0100

    completed blas support
    works with openblas:
    
    aptitude install libopenblas-dev

--- src/LinearAlgebra/wraps/blas/blas.hpp
+++ src/LinearAlgebra/wraps/blas/blas.hpp
@@ -7,5 +7,6 @@
 #if (HAS_BLAS)
 #include "LinearAlgebra/wraps/blas/subroutines.hpp"
 #include "LinearAlgebra/wraps/blas/to_cblas_transpose.hpp"
+#include "LinearAlgebra/wraps/blas/is_cblas_supported_scalar.hpp"
 #endif
 

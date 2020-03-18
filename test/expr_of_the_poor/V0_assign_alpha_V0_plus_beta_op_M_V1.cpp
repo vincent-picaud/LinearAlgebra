da4e9a02026516e915eddd67e236b1d7f0fcb58f
picaud <picaud@am-080027980deb.intern>
2020-03-18 10:24:59 +0100

    add mv test

---
+++ test/expr_of_the_poor/V0_assign_alpha_V0_plus_beta_op_M_V1.cpp
@@ -0,0 +1,20 @@
+#include "LinearAlgebra/expr_of_the_poor/V0_assign_alpha_V0_plus_beta_op_M_V1.hpp"
+#include "LinearAlgebra/dense/matrix.hpp"
+#include "LinearAlgebra/dense/vector.hpp"
+
+#include <gtest/gtest.h>
+
+using namespace LinearAlgebra;
+
+TEST(Known_Pattern, copy)
+{
+  using T = double;
+
+  Tiny_Matrix<T, 3, 2> M;
+  Vector<T> y(3);
+  Tiny_Vector<T, 2> x;
+
+  Expr_Selector_Enum selected = expr(y, _assign_, 1, _vector_0_, _plus_, 2, _identity_, M, x);
+
+  EXPECT_EQ(selected, Expr_Selector_Enum::Blas);
+}

2bac7a05dbf1d3b0191f897c59f029d2d8ffc118
picaud <picaud@am-080027980deb.intern>
2020-03-18 11:14:58 +0100

    return type mais bug

--- examples/expr_usage_2.cpp
+++ examples/expr_usage_2.cpp
@@ -12,17 +12,35 @@ main()
   //  using T = std::complex<double>;
   using T = double;
 
-  Tiny_Matrix<T, 5, 2> M;
-  Vector<T> y(5), x(2);
+  Tiny_Matrix<T, 3, 2> M;
+  Vector<T> y(3);
+  Tiny_Vector<T, 2> x;
 
+  M(0, 0) = 1;
+  M(0, 1) = 0;
+  M(1, 0) = 0;
+  M(1, 1) = 1;
+  M(2, 0) = 1;
+  M(2, 1) = 1;
+
+  x[0] = 1;
+  x[1] = 2;
   //  expr(y, _assign_, 1, _identity_, M, x, _plus_, 2, _vector_0_);
 
 #if (HAS_BLAS)
-  expr(y, _assign_, 1, _vector_0_, _plus_, 2, _identity_, M, x);
+  expr(y, _assign_, 0, _vector_0_, _plus_, 2, _identity_, M, x);
   //  expr(y, _assign_, 1, _conjugate_, M, x, _plus_, 2, _vector_0_);
 #else
   assert(0 && "Not implemented yet");
 #endif
 
-  std::cout << y;
+  std::cout << y << std::endl;
+  std::cout << M << std::endl;
+  std::cout << x << std::endl;
+
+  std::cout << "y =" << y[0] << " " << y[1] << " " << y[2] << std::endl;
+  std::cout << "x =" << x[0] << " " << x[1] << std::endl;
+  assert(y[0] == 2 * x[0]);
+  assert(y[1] == 2 * x[1]);
+  assert(y[2] == 2 * (x[0] + x[1]));
 }
--- test/expr_of_the_poor/V0_assign_alpha_V0_plus_beta_op_M_V1.cpp
+++ test/expr_of_the_poor/V0_assign_alpha_V0_plus_beta_op_M_V1.cpp
@@ -6,16 +6,29 @@
 
 using namespace LinearAlgebra;
 
-TEST(Known_Pattern, copy)
+TEST(V0_assign_alpha_V0_plus_beta_op_M_V1,basic)
 {
   using T = double;
 
-  
   Tiny_Matrix<T, 3, 2> M;
   Vector<T> y(3);
   Tiny_Vector<T, 2> x;
 
-  Expr_Selector_Enum selected = expr(y, _assign_, 1, _vector_0_, _plus_, 2, _identity_, M, x);
+  M(0, 0) = 1;
+  M(0, 1) = 0;
+  M(1, 0) = 0;
+  M(1, 1) = 1;
+  M(2, 0) = 1;
+  M(2, 1) = 1;
+
+  x[0] = 1;
+  x[1] = 2;
+
+  Expr_Selector_Enum selected = expr(y, _assign_, 0, _vector_0_, _plus_, 2, _identity_, M, x);
 
   EXPECT_EQ(selected, Expr_Selector_Enum::Blas);
+
+  EXPECT_EQ(y[0], 2 * x[0]);
+  EXPECT_EQ(y[1], 2 * x[1]);
+  EXPECT_EQ(y[2], 2 * (x[0] + x[1]));
 }

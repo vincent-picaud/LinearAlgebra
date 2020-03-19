d9a6679d4e5c35c591fae9d13b142c40c6518ecd
picaud <picaud@am-080027980deb.intern>
2020-03-19 23:00:18 +0100

    remove deprecated inplace_map

--- src/LinearAlgebra/dense/matrix_map.hpp
+++ src/LinearAlgebra/dense/matrix_map.hpp
@@ -8,27 +8,19 @@
 
 namespace LinearAlgebra
 {
-  // CAVEAT: deprecated
-  template <typename LAMBDA, typename IMPL_DEST, typename IMPL_SRC>
-  void
-  inplace_map(const LAMBDA& lambda, Dense_Matrix_Crtp<IMPL_DEST>& matrix_dest,
-              const Dense_Matrix_Crtp<IMPL_SRC>& matrix_src)
-  {
-    const auto loop_over_indices_lambda = [&](const std::size_t i, const std::size_t j) {
-      matrix_dest(i, j) = lambda(matrix_src.as_const()(i, j));
-    };
-    matrix_dest.storage_scheme().loop_over_indices(loop_over_indices_lambda);
-  }
-
   template <typename LAMBDA, typename IMPL_SRC>
   auto
   map(const LAMBDA& lambda, const Dense_Matrix_Crtp<IMPL_SRC>& matrix_src)
   {
     using matrix_dest_element_type = decltype(lambda(matrix_src(0, 0)));
-
     auto matrix_dest = create_default_storable(type_v<matrix_dest_element_type>, matrix_src.impl());
 
-    inplace_map(lambda, matrix_dest, matrix_src);
+    //----------------
+
+    const auto loop_over_indices_lambda = [&](const std::size_t i, const std::size_t j) {
+      matrix_dest(i, j) = lambda(matrix_src.as_const()(i, j));
+    };
+    matrix_dest.storage_scheme().loop_over_indices(loop_over_indices_lambda);
 
     return matrix_dest;
   }
--- src/LinearAlgebra/dense/vector_map.hpp
+++ src/LinearAlgebra/dense/vector_map.hpp
@@ -8,26 +8,19 @@
 
 namespace LinearAlgebra
 {
-  template <typename LAMBDA, typename IMPL_DEST, typename IMPL_SRC>
-  void
-  inplace_map(const LAMBDA& lambda, Dense_Vector_Crtp<IMPL_DEST>& vector_dest,
-              const Dense_Vector_Crtp<IMPL_SRC>& vector_src)
-  {
-    const auto loop_over_indices_lambda = [&](const std::size_t i) {
-      vector_dest[i] = lambda(vector_src.as_const()[i]);
-    };
-    vector_dest.storage_scheme().loop_over_indices(loop_over_indices_lambda);
-  }
-
   template <typename LAMBDA, typename IMPL_SRC>
   auto
   map(const LAMBDA& lambda, const Dense_Vector_Crtp<IMPL_SRC>& vector_src)
   {
     using vector_dest_element_type = decltype(lambda(vector_src[0]));
-
     auto vector_dest = create_default_storable(type_v<vector_dest_element_type>, vector_src.impl());
 
-    inplace_map(lambda, vector_dest, vector_src);
+    //----------------
+
+    const auto loop_over_indices_lambda = [&](const std::size_t i) {
+      vector_dest[i] = lambda(vector_src.as_const()[i]);
+    };
+    vector_dest.storage_scheme().loop_over_indices(loop_over_indices_lambda);
 
     return vector_dest;
   }

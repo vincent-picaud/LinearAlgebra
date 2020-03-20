56f998d2af9b9aed36a09d384e25736b37fb311a
picaud <picaud@am-080027980deb.intern>
2020-03-20 10:33:11 +0100

    fix vector_map

--- src/LinearAlgebra/dense/vector_create_default_storable.hpp
+++ src/LinearAlgebra/dense/vector_create_default_storable.hpp
@@ -6,24 +6,39 @@
 #pragma once
 
 #include "LinearAlgebra/dense/vector.hpp"
+#include "LinearAlgebra/meta/element_type.hpp"
+#include "LinearAlgebra/meta/size_utils.hpp"
 #include "LinearAlgebra/meta/type.hpp"
 
 namespace LinearAlgebra
 {
-  template <typename T, typename IMPL>
+  // Create default a storable vector from vector
+  //
+  // If IMPL_OPTIONAL is not empty, checks that all these vector have
+  // a compatible structure and try to use any static size if any.
+  //
+  template <typename T, typename IMPL, typename... IMPL_OPTIONAL>
   auto
-  create_default_storable(const Type<T>, const Dense_Vector_Crtp<IMPL>& vector)
+  create_default_storable(const Type<T>, const Dense_Vector_Crtp<IMPL>& vector,
+                          const Dense_Vector_Crtp<IMPL_OPTIONAL>&... vector_optional)
   {
-    using vector_type =
-        Default_Vector<T, typename IMPL::size_type, std::integral_constant<std::size_t, 1>>;
+    assert(all_sizes_are_equal_p(vector.size(), vector_optional.size()...));
 
-    return vector_type{vector.size()};
+    auto vector_dest_size = get_static_size_if_any(vector.size(), vector_optional.size()...);
+
+    using vector_type =
+        Default_Vector<T, decltype(vector_dest_size), std::integral_constant<std::size_t, 1>>;
+
+    return vector_type{vector_dest_size};
   }
 
-  template <typename IMPL>
+  // as before but uses std::common<...> result to select "element_type"
+  template <typename IMPL, typename... IMPL_OPTIONAL>
   auto
-  create_default_storable(const Dense_Vector_Crtp<IMPL>& vector)
+  create_default_storable(const Dense_Vector_Crtp<IMPL>& vector,
+                          const Dense_Vector_Crtp<IMPL_OPTIONAL>&... vector_optional)
   {
-    return create_default_storable(type_v<typename IMPL::element_type>, vector);
+    return create_default_storable(type_v<Common_Element_Type_t<IMPL, IMPL_OPTIONAL...>>, vector,
+                                   vector_optional...);
   }
 }
--- src/LinearAlgebra/dense/vector_map.hpp
+++ src/LinearAlgebra/dense/vector_map.hpp
@@ -5,21 +5,29 @@
 
 #include "LinearAlgebra/dense/vector.hpp"
 #include "LinearAlgebra/dense/vector_create_default_storable.hpp"
+#include "LinearAlgebra/meta/size_utils.hpp"
 
 namespace LinearAlgebra
 {
-  template <typename LAMBDA, typename IMPL_SRC>
+  template <typename LAMBDA, typename IMPL_SRC, typename... IMPL_SRC_OPTIONAL>
   auto
-  map(const LAMBDA& lambda, const Dense_Vector_Crtp<IMPL_SRC>& vector_src)
+  map(const LAMBDA& lambda, const Dense_Vector_Crtp<IMPL_SRC>& vector_src,
+      const Dense_Vector_Crtp<IMPL_SRC_OPTIONAL>&... vector_src_optional)
   {
-    using vector_dest_element_type = decltype(lambda(vector_src[0]));
-    auto vector_dest = create_default_storable(type_v<vector_dest_element_type>, vector_src.impl());
+    auto vector_dest = create_default_storable(
+        type_v<decltype(lambda(vector_src.as_const()[0], vector_src_optional.as_const()[0]...))>,
+        vector_src, vector_src_optional...);
 
     //----------------
 
     const auto loop_over_indices_lambda = [&](const std::size_t i) {
-      vector_dest[i] = lambda(vector_src.as_const()[i]);
+      vector_dest[i] = lambda(vector_src.as_const()[i], vector_src_optional.as_const()[i]...);
     };
+
+    // Note: create_default_storable() has already selected
+    //       vector_dest with a static size (if possible) hence we can
+    //       use vector_dest for loop_over_indices().
+    //
     vector_dest.storage_scheme().loop_over_indices(loop_over_indices_lambda);
 
     return vector_dest;

3b1676a510be507d1e025e1ccf3a5a1eb23c613d
picaud <picaud@am-080027980deb.intern>
2020-03-20 20:17:03 +0100

    clean template

--- src/LinearAlgebra/expr/X_assign_XXX_template.hpp
+++ src/LinearAlgebra/expr/X_assign_XXX_template.hpp
@@ -1,23 +1,15 @@
-// Role: this is only a template, not included in the meson.build file
+// CAVEAT: this is a TEMPLATE (not compiled, not exported) 
 //
 // Compute:
-
-
+//
 #pragma once
 
 #include "LinearAlgebra/dense/matrix_crtp_fwd.hpp"
-#include "LinearAlgebra/dense/matrix_special_structure_enum.hpp"
 #include "LinearAlgebra/dense/vector_crtp_fwd.hpp"
-#include "LinearAlgebra/expr/dimension.hpp"
+
 #include "LinearAlgebra/expr/expr_selector.hpp"
 #include "LinearAlgebra/expr/expr_tags.hpp"
-
-#include "LinearAlgebra/utils/all_same_type.hpp"
-#include "LinearAlgebra/utils/element_type.hpp"
-
-#include "LinearAlgebra/blas/is_cblas_supported_scalar.hpp"
-#include "LinearAlgebra/blas/subroutines.hpp"
-#include "LinearAlgebra/blas/to_cblas_transpose.hpp"
+#include "LinearAlgebra/utils/always.hpp"
 
 namespace LinearAlgebra
 {

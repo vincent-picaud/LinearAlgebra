8967e7735577fbaa261ab56c5c67e5e8e2b0e34d
picaud <picaud@am-080027980deb.intern>
2020-03-21 15:56:28 +0100

    first test with views

--- src/LinearAlgebra/dense/vector_view.hpp
+++ src/LinearAlgebra/dense/vector_view.hpp
@@ -1,9 +1,103 @@
 // Role: create vector views
 //
 // CAVEAT: for views our convention is *always* to use [begin,end[ and NOT (begin,size)
-//         by example view(2,4) mean {v_2,v_3}, begin=2, end=4 (and NOT {v_2,v_3,v_4,v_5}, begin=2, size=4)) 
+//         by example view(2,4) mean {v_2,v_3}, begin=2, end=4 (and NOT {v_2,v_3,v_4,v_5}, begin=2, size=4))
 #pragma once
 
-namespace LinearAlgebra {
+#include "LinearAlgebra/dense/vector.hpp"
+#include "LinearAlgebra/utils/is_std_integral_constant.hpp"
 
+namespace LinearAlgebra
+{
+  // Some utils
+  //////////////////////////////////////////////////////////////////
+  //
+  // -> a priori to move to utils/
+  //
+  template <typename BEGIN, typename END>
+  bool
+  check_size_begin_end_p(const std::size_t size, const BEGIN begin, const END end) noexcept
+  {
+    bool ok = true;
+    ok &= Is_Std_Integral_Constant_Size_Or_Std_Size_v<BEGIN>;
+    ok &= Is_Std_Integral_Constant_Size_Or_Std_Size_v<END>;
+    ok &= 0 <= begin;
+    ok &= begin <= end;
+    ok &= end <= size;
+    return ok;
+  }
+
+  //================================================================
+
+  std::size_t
+  compute_size_from_begin_end(const std::size_t begin, const std::size_t end) noexcept
+  {
+    assert(begin <= end);
+
+    return end - begin;
+  }
+  template <std::size_t BEGIN, std::size_t END>
+  std::integral_constant<std::size_t, END - BEGIN>
+  compute_size_from_begin_end(const std::integral_constant<std::size_t, BEGIN>,
+                              const std::integral_constant<std::size_t, END>) noexcept
+  {
+    static_assert(BEGIN <= END);
+
+    return {};
+  }
+
+  //================================================================
+
+  // Vector views
+  //////////////////////////////////////////////////////////////////
+  //
+
+  // Note: we are constrained to filter arguments by a two-steps approach, otherwise calling
+  //
+  // view(v,1,2)
+  //
+  // gives BEGIN=int, END=int
+  //
+  // Hopefully as soon as either one of BEGIN or END is of type size_t
+  // the size is dynamic, hence the initial 4 cases:
+  //
+  // BEGIN                                       | END
+  // std::integral_constant<std::size_t, BEGIN>  | std::integral_constant<std::size_t, END>
+  // std::integral_constant<std::size_t, BEGIN>  | std::size_t
+  // std::size_t                                 | std::integral_constant<std::size_t, END>
+  // std::size_t                                 | std::size_t
+  //
+  // reduce to 2 cases:
+  //   1/ both   BEGIN, END are std::integral_constant<std::size_t, ... >
+  //   2/ one of BEGIN, END is std::size_t
+  //
+  namespace Detail
+  {
+    template <typename IMPL, typename BEGIN, typename END>
+    auto
+    view(Dense_Vector_Crtp<IMPL>& vector, const BEGIN begin, const END end) noexcept
+    {
+      assert(check_size_begin_end_p(vector.size(), begin, end));
+
+      auto size = compute_size_from_begin_end(begin, end);
+
+      return Default_Vector_View<typename IMPL::element_type, decltype(size),
+                                 typename IMPL::increment_type>(vector.data() + begin, size,
+                                                                vector.increment());
+    }
+  }
+
+  template <typename IMPL, std::size_t BEGIN, std::size_t END>
+  auto
+  view(Dense_Vector_Crtp<IMPL>& vector, const std::integral_constant<std::size_t, BEGIN> begin,
+       const std::integral_constant<std::size_t, END> end) noexcept
+  {
+    return Detail::view(vector, begin, end);
+  }
+  template <typename IMPL>
+  auto
+  view(Dense_Vector_Crtp<IMPL>& vector, const std::size_t begin, const std::size_t end) noexcept
+  {
+    return Detail::view(vector, begin, end);
+  }
 }
--- test/LinearAlgebra/dense/vector_view.cpp
+++ test/LinearAlgebra/dense/vector_view.cpp
@@ -1,5 +1,5 @@
 #include "LinearAlgebra/dense/vector_view.hpp"
-#include "LinearAlgebra/dense/vector.hpp"
+#include "LinearAlgebra/expr/vector_expr.hpp"
 
 #include <gtest/gtest.h>
 
@@ -8,6 +8,12 @@ using namespace LinearAlgebra;
 TEST(Tiny_Vector_View, Constructor)
 {
   Tiny_Vector<int, 10> v;
-
+  expr(v, _assign_, 1);
   auto view_v = view(v, 1, 3);
+  expr(view_v, _assign_, 2);
+
+  EXPECT_EQ(v[0], 1);
+  EXPECT_EQ(v[1], 2);
+  EXPECT_EQ(v[2], 2);
+  EXPECT_EQ(v[3], 1);
 }

1b24a698979a4581c3eb688ade01bad7accf322e
picaud <picaud@am-080027980deb.intern>
2020-03-21 16:40:26 +0100

    const vector view test

--- test/LinearAlgebra/dense/vector_view.cpp
+++ test/LinearAlgebra/dense/vector_view.cpp
@@ -80,14 +80,12 @@ TEST(Vector_View, constness_check)
 {
   Tiny_Vector<int, 10> v;
   expr(v, _assign_, 1);
+  v[1] = 2;
   auto view_v = view(v.as_const(), 1, 3);
-  expr(view_v, _assign_, 2);
 
   EXPECT_TRUE((std::is_same_v<std::size_t, typename decltype(view_v)::size_type>));
   EXPECT_EQ(view_v.size(), 2);
 
-  EXPECT_EQ(v[0], 1);
-  EXPECT_EQ(v[1], 2);
-  EXPECT_EQ(v[2], 2);
-  EXPECT_EQ(v[3], 1);
+  EXPECT_EQ(view_v[0], 2);
+  EXPECT_EQ(view_v[1], 1);
 }

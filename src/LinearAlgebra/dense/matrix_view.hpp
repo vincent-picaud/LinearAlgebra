b836e67464ad2e485b0ee804fee05e350d84105c
picaud <picaud@am-080027980deb.intern>
2020-03-22 10:14:36 +0100

    add const view impl + test

--- src/LinearAlgebra/dense/matrix_view.hpp
+++ src/LinearAlgebra/dense/matrix_view.hpp
@@ -32,7 +32,28 @@ namespace LinearAlgebra
           &matrix(I_begin, J_begin), I_size, J_size,  // CAVEAT: and not data()+I_begin
           matrix.leading_dimension());  //         which does not take into account increment
     }
-    // TODO: const view
+    // Const view version: see vector_view.hpp for extra comments
+    template <typename IMPL, typename I_BEGIN, typename I_END, typename J_BEGIN, typename J_END>
+    auto
+    view(const Dense_Matrix_Crtp<IMPL>& matrix, const I_BEGIN I_begin, const I_END I_end,
+         const J_BEGIN J_begin, const J_END J_end) noexcept
+    {
+      assert(check_size_begin_end_p(matrix.I_size(), I_begin, I_end));
+      assert(check_size_begin_end_p(matrix.J_size(), J_begin, J_end));
+      assert(IMPL::storage_scheme_type::matrix_storage_mask_type::value ==
+                 Matrix_Storage_Mask_Enum::None or
+             (I_begin == J_begin and I_end == J_end));
+
+      auto I_size = compute_size_from_begin_end(I_begin, I_end);
+      auto J_size = compute_size_from_begin_end(J_begin, J_end);
+
+      return Default_Matrix_Const_View<
+          typename IMPL::element_type, IMPL::matrix_special_structure_type::value,
+          IMPL::storage_scheme_type::matrix_storage_mask_type::value, decltype(I_size),
+          decltype(J_size), typename IMPL::leading_dimension_type>(
+          &matrix(I_begin, J_begin), I_size, J_size,  // CAVEAT: and not data()+I_begin
+          matrix.leading_dimension());  //         which does not take into account increment
+    }
   }
 
   // CAVEAT: when matrix has a peculiar structure: triangular,
@@ -76,12 +97,45 @@ namespace LinearAlgebra
   {
     return Detail::view(matrix, I_begin, I_end, J_begin, J_end);
   }
-  // template <typename IMPL, typename I_BEGIN, typename I_END, typename J_BEGIN, typename J_END>
-  // auto
-  // view(Dense_Matrix_Crtp<IMPL>& matrix, const I_BEGIN I_begin, const I_END I_end,
-  //      const J_BEGIN J_begin, const J_END J_end) noexcept
+  // Const view versions: only add a const to matrix argument
+  //
+  template <typename IMPL>
+  auto
+  view(const Dense_Matrix_Crtp<IMPL>& matrix, const std::size_t I_begin, const std::size_t I_end,
+       const std::size_t J_begin, const std::size_t J_end) noexcept
 
-  // {
-  //   return Detail::view(vector, begin, end);
-  // }
+  {
+    return Detail::view(matrix, I_begin, I_end, J_begin, J_end);
+  }
+  template <typename IMPL, std::size_t I_BEGIN, std::size_t I_END>
+  auto
+  view(const Dense_Matrix_Crtp<IMPL>& matrix,
+       const std::integral_constant<std::size_t, I_BEGIN> I_begin,
+       const std::integral_constant<std::size_t, I_END> I_end, const std::size_t J_begin,
+       const std::size_t J_end) noexcept
+
+  {
+    return Detail::view(matrix, I_begin, I_end, J_begin, J_end);
+  }
+  template <typename IMPL, std::size_t J_BEGIN, std::size_t J_END>
+  auto
+  view(const Dense_Matrix_Crtp<IMPL>& matrix, const std::size_t I_begin, const std::size_t I_end,
+       const std::integral_constant<std::size_t, J_BEGIN> J_begin,
+       const std::integral_constant<std::size_t, J_END> J_end) noexcept
+
+  {
+    return Detail::view(matrix, I_begin, I_end, J_begin, J_end);
+  }
+  template <typename IMPL, std::size_t I_BEGIN, std::size_t I_END, std::size_t J_BEGIN,
+            std::size_t J_END>
+  auto
+  view(const Dense_Matrix_Crtp<IMPL>& matrix,
+       const std::integral_constant<std::size_t, I_BEGIN> I_begin,
+       const std::integral_constant<std::size_t, I_END> I_end,
+       const std::integral_constant<std::size_t, J_BEGIN> J_begin,
+       const std::integral_constant<std::size_t, J_END> J_end) noexcept
+
+  {
+    return Detail::view(matrix, I_begin, I_end, J_begin, J_end);
+  }
 }
--- test/LinearAlgebra/dense/matrix_view.cpp
+++ test/LinearAlgebra/dense/matrix_view.cpp
@@ -34,6 +34,8 @@ TEST(Matrix_View, static_of_static_I)
                        std::integral_constant<std::size_t, 3>(), 2, 5);
   expr(view_mat, _assign_, 2);
 
+  EXPECT_TRUE((std::is_same_v<int&, decltype(view_mat(0, 0))>));
+
   EXPECT_TRUE((std::is_same_v<std::integral_constant<std::size_t, 2>,
                               typename decltype(view_mat)::I_size_type>));
   EXPECT_TRUE((std::is_same_v<std::size_t, typename decltype(view_mat)::J_size_type>));
@@ -89,6 +91,36 @@ TEST(Matrix_View, static_of_static_IJ)
                               typename decltype(view_mat)::I_size_type>));
   EXPECT_TRUE((std::is_same_v<std::integral_constant<std::size_t, 3>,
                               typename decltype(view_mat)::J_size_type>));
+
+  EXPECT_EQ(view_mat.I_size(), 2);
+  EXPECT_EQ(view_mat.J_size(), 3);
+
+  EXPECT_EQ(mat(0, 2), 1);
+  EXPECT_EQ(mat(1, 2), 2);
+  EXPECT_EQ(mat(1, 2), 2);
+  EXPECT_EQ(mat(1, 1), 1);
+
+  EXPECT_EQ(mat(2, 5), 1);
+  EXPECT_EQ(mat(2, 4), 2);
+  EXPECT_EQ(mat(3, 4), 1);
+}
+// Const version
+TEST(Matrix_View, const_static_of_static_I)
+{
+  Tiny_Matrix<int, 5, 6> mat;
+  expr(mat, _assign_, 1);
+  auto view_mat_mutable = view(mat, std::integral_constant<std::size_t, 1>(),
+                               std::integral_constant<std::size_t, 3>(), 2, 5);
+  expr(view_mat_mutable, _assign_, 2);
+
+  auto view_mat = view(mat.as_const(), std::integral_constant<std::size_t, 1>(),
+                       std::integral_constant<std::size_t, 3>(), 2, 5);
+
+  EXPECT_TRUE((std::is_same_v<const int&, decltype(view_mat(0, 0))>));
+
+  EXPECT_TRUE((std::is_same_v<std::integral_constant<std::size_t, 2>,
+                              typename decltype(view_mat)::I_size_type>));
+  EXPECT_TRUE((std::is_same_v<std::size_t, typename decltype(view_mat)::J_size_type>));
 
   EXPECT_EQ(view_mat.I_size(), 2);
   EXPECT_EQ(view_mat.J_size(), 3);

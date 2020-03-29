750ec820b89685338a74bbd3d7c31dbf77dad274
picaud <picaud@am-080027980deb.intern>
2020-03-29 12:50:48 +0200

    fix buggy vector scan implementation

--- src/LinearAlgebra/dense/vector_scan.hpp
+++ src/LinearAlgebra/dense/vector_scan.hpp
@@ -20,7 +20,7 @@ namespace LinearAlgebra
     assert(are_compatible_p(vector, vector_optional...));
 
     const auto loop_over_indices_lambda = [&](const std::size_t i) {
-      vector[i] = lambda(vector[i], vector_optional[i]...);
+      lambda(vector[i], vector_optional[i]...);
     };
 
     // For efficiency, try to catch a static size if any
@@ -40,11 +40,33 @@ namespace LinearAlgebra
     assert(are_compatible_p(vector, vector_optional...));
 
     const auto loop_over_indices_lambda = [&](const std::size_t i) {
-      vector[i] = lambda(i, vector[i], vector_optional[i]...);
+      lambda(i, vector[i], vector_optional[i]...);
     };
     // For efficiency, try to catch a static size if any
     auto vector_dest_size = get_static_size_if_any(vector.size(), vector_optional.size()...);
 
     Detail::loop_over_indices(loop_over_indices_lambda, vector_dest_size);
   }
+
+  //////////////////////////////////////////////////////////////////
+
+  //
+  // lambda(V[i],Vopt1[i],Vopt2[i]...) everything is const
+  //
+  // template <typename LAMBDA, typename IMPL, typename... IMPL_OPTIONAL>
+  // book
+  // scan_while(const LAMBDA& lambda, const Dense_Vector_Crtp<IMPL>& vector,
+  //            const Dense_Vector_Crtp<IMPL_OPTIONAL>&... vector_optional)
+  // {
+  //   assert(are_compatible_p(vector, vector_optional...));
+
+  //   const auto loop_over_indices_lambda = [&](const std::size_t i) {
+  //     vector[i] = lambda(vector[i], vector_optional[i]...);
+  //   };
+
+  //   // For efficiency, try to catch a static size if any
+  //   auto vector_dest_size = get_static_size_if_any(vector.size(), vector_optional.size()...);
+
+  //   Detail::loop_over_indices(loop_over_indices_lambda, vector_dest_size);
+  // }
 }

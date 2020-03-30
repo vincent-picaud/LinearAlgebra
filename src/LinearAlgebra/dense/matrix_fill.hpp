08eff315ec72f8e434f797886e8c7e17b6e113f5
picaud <picaud@am-080027980deb.intern>
2020-03-30 18:43:41 +0200

    now fill support additional arg like transform

--- src/LinearAlgebra/dense/matrix_fill.hpp
+++ src/LinearAlgebra/dense/matrix_fill.hpp
@@ -3,33 +3,51 @@
 //
 #pragma once
 
+#include "LinearAlgebra/dense/matrix_are_compatible.hpp"
 #include "LinearAlgebra/dense/matrix_crtp_fwd.hpp"
+#include "LinearAlgebra/utils/size_utils.hpp"
 
 namespace LinearAlgebra
 {
   //
-  // M(i,j)=lambda()
+  // M(i,j)=lambda(Mopt(i,j)...)
   //
-  template <typename LAMBDA, typename IMPL>
+  template <typename LAMBDA, typename IMPL, typename... IMPL_OPTIONAL>
   void
-  fill(const LAMBDA& lambda, Dense_Matrix_Crtp<IMPL>& matrix)
+  fill(const LAMBDA& lambda, Dense_Matrix_Crtp<IMPL>& matrix,
+       const Dense_Matrix_Crtp<IMPL_OPTIONAL>&... matrix_optional)
   {
+    assert(are_compatible_p(matrix, matrix_optional...));
+
     const auto loop_over_indices_lambda = [&](const std::size_t i, const std::size_t j) {
-      matrix(i, j) = lambda();
+      matrix(i, j) = lambda(matrix_optional(i, j)...);
     };
-    matrix.storage_scheme().loop_over_indices(loop_over_indices_lambda);
+    // For efficiency, try to catch a static size if any
+    auto matrix_dest_I_size = get_static_size_if_any(matrix.I_size(), matrix_optional.I_size()...);
+    auto matrix_dest_J_size = get_static_size_if_any(matrix.J_size(), matrix_optional.J_size()...);
+
+    Detail::loop_over_indices(loop_over_indices_lambda,
+                              typename IMPL::storage_scheme_type::matrix_storage_mask_type(),
+                              matrix_dest_I_size, matrix_dest_J_size);
   }
 
   //
-  // M(i,j)=lambda(i,j)
+  // M(i,j)=lambda(i,j,Mopt(i,j)...)
   //
-  template <typename LAMBDA, typename IMPL>
+  template <typename LAMBDA, typename IMPL, typename... IMPL_OPTIONAL>
   void
-  fill_indexed(const LAMBDA& lambda, Dense_Matrix_Crtp<IMPL>& matrix)
+  fill_indexed(const LAMBDA& lambda, Dense_Matrix_Crtp<IMPL>& matrix,
+               const Dense_Matrix_Crtp<IMPL_OPTIONAL>&... matrix_optional)
   {
     const auto loop_over_indices_lambda = [&](const std::size_t i, const std::size_t j) {
-      matrix(i, j) = lambda(i, j);
+      matrix(i, j) = lambda(i, j, matrix_optional(i, j)...);
     };
-    matrix.storage_scheme().loop_over_indices(loop_over_indices_lambda);
+    // For efficiency, try to catch a static size if any
+    auto matrix_dest_I_size = get_static_size_if_any(matrix.I_size(), matrix_optional.I_size()...);
+    auto matrix_dest_J_size = get_static_size_if_any(matrix.J_size(), matrix_optional.J_size()...);
+
+    Detail::loop_over_indices(loop_over_indices_lambda,
+                              typename IMPL::storage_scheme_type::matrix_storage_mask_type(),
+                              matrix_dest_I_size, matrix_dest_J_size);
   }
 }
--- src/LinearAlgebra/dense/vector_fill.hpp
+++ src/LinearAlgebra/dense/vector_fill.hpp
@@ -3,29 +3,64 @@
 //
 #pragma once
 
+#include "LinearAlgebra/dense/vector_are_compatible.hpp"
 #include "LinearAlgebra/dense/vector_crtp_fwd.hpp"
+#include "LinearAlgebra/utils/size_utils.hpp"
 
 namespace LinearAlgebra
 {
   //
   // V[i]=lambda()
   //
-  template <typename LAMBDA, typename IMPL>
+  // template <typename LAMBDA, typename IMPL>
+  // void
+  // fill(const LAMBDA& lambda, Dense_Vector_Crtp<IMPL>& vector)
+  // {
+  //   const auto loop_over_indices_lambda = [&](const std::size_t i) { vector[i] = lambda(); };
+  //   vector.storage_scheme().loop_over_indices(loop_over_indices_lambda);
+  // }
+
+  //
+  // V[i]=lambda(Vopt1[i],Vopt2[i]...)
+  //
+  // Note: the difference with transform is:
+  //
+  //       transform: V[i]=lambda(V[i],Vopt1[i],Vopt2[i]...)
+  //       fill:      V[i]=lambda(     Vopt1[i],Vopt2[i]...)
+  //
+  template <typename LAMBDA, typename IMPL, typename... IMPL_OPTIONAL>
   void
-  fill(const LAMBDA& lambda, Dense_Vector_Crtp<IMPL>& vector)
+  fill(const LAMBDA& lambda, Dense_Vector_Crtp<IMPL>& vector,
+       const Dense_Vector_Crtp<IMPL_OPTIONAL>&... vector_optional)
   {
-    const auto loop_over_indices_lambda = [&](const std::size_t i) { vector[i] = lambda(); };
-    vector.storage_scheme().loop_over_indices(loop_over_indices_lambda);
+    assert(are_compatible_p(vector, vector_optional...));
+
+    const auto loop_over_indices_lambda = [&](const std::size_t i) {
+      vector[i] = lambda(vector_optional[i]...);
+    };
+
+    // For efficiency, try to catch a static size if any
+    auto vector_dest_size = get_static_size_if_any(vector.size(), vector_optional.size()...);
+
+    Detail::loop_over_indices(loop_over_indices_lambda, vector_dest_size);
   }
 
   //
-  // V[i]=lambda(i)
+  // V[i]=lambda(i,Vopt1[i],Vopt2[i]...)
   //
-  template <typename LAMBDA, typename IMPL>
+  template <typename LAMBDA, typename IMPL, typename... IMPL_OPTIONAL>
   void
-  fill_indexed(const LAMBDA& lambda, Dense_Vector_Crtp<IMPL>& vector)
+  fill_indexed(const LAMBDA& lambda, Dense_Vector_Crtp<IMPL>& vector,
+               const Dense_Vector_Crtp<IMPL_OPTIONAL>&... vector_optional)
   {
-    const auto loop_over_indices_lambda = [&](const std::size_t i) { vector[i] = lambda(i); };
-    vector.storage_scheme().loop_over_indices(loop_over_indices_lambda);
+    assert(are_compatible_p(vector, vector_optional...));
+
+    const auto loop_over_indices_lambda = [&](const std::size_t i) {
+      vector[i] = lambda(i, vector_optional[i]...);
+    };
+    // For efficiency, try to catch a static size if any
+    auto vector_dest_size = get_static_size_if_any(vector.size(), vector_optional.size()...);
+
+    Detail::loop_over_indices(loop_over_indices_lambda, vector_dest_size);
   }
 }

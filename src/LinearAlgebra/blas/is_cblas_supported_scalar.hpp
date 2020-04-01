f1d775f45bd5c4323376dc4dc47981d2c1f20f7f
picaud <picaud@am-080027980deb.intern>
2020-04-01 17:59:40 +0200

    split apart real & complex case

--- src/LinearAlgebra/blas/is_cblas_supported_scalar.hpp
+++ src/LinearAlgebra/blas/is_cblas_supported_scalar.hpp
@@ -9,11 +9,38 @@
 
 namespace LinearAlgebra
 {
+  // Split Real & Complex cases
+  //================================================================
+
+  // Real
+  //
+  template <typename T>
+  struct Is_CBlas_Supported_Real_Scalar
+      : std::integral_constant<bool, std::is_same_v<float, T> or std::is_same_v<double, T>>
+  {
+  };
+
+  template <typename T>
+  constexpr auto Is_CBlas_Supported_Real_Scalar_v = Is_CBlas_Supported_Real_Scalar<T>::value;
+
+  // Complex
+  //
+  template <typename T>
+  struct Is_CBlas_Supported_Complex_Scalar
+      : std::integral_constant<bool, std::is_same_v<std::complex<float>, T> or
+                                         std::is_same_v<std::complex<double>, T>>
+  {
+  };
+
+  template <typename T>
+  constexpr auto Is_CBlas_Supported_Complex_Scalar_v = Is_CBlas_Supported_Complex_Scalar<T>::value;
+
+  // Real or Complex
+  //
   template <typename T>
   struct Is_CBlas_Supported_Scalar
-      : std::integral_constant<bool, std::is_same_v<float, T> or std::is_same_v<double, T> or
-                                         std::is_same_v<std::complex<float>, T> or
-                                         std::is_same_v<std::complex<double>, T> >
+      : std::integral_constant<bool, Is_CBlas_Supported_Real_Scalar_v<T> or
+                                         Is_CBlas_Supported_Complex_Scalar_v<T>>
   {
   };
 

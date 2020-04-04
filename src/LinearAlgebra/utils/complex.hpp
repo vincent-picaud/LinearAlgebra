727741e2fb3cb2811d1e6bae46ed46ebb4103766
picaud <picaud@am-080027980deb.intern>
2020-04-04 11:23:29 +0200

    extend complex support

--- src/LinearAlgebra/utils/complex.hpp
+++ src/LinearAlgebra/utils/complex.hpp
@@ -13,9 +13,28 @@
 #include "LinearAlgebra/utils/type_identity.hpp"
 
 #include <ccomplex>
+#include <type_traits>
 
 namespace LinearAlgebra
 {
+  // Is_Complex
+  //////////////////////////////////////////////////////////////////
+  //
+  template <typename T>
+  struct Is_Complex : std::false_type
+  {
+  };
+  template <typename T>
+  struct Is_Complex<std::complex<T>> : std::true_type
+  {
+  };
+
+  template <typename T>
+  constexpr auto Is_Complex_v = Is_Complex<T>::value;
+
+  // Comparison operators
+  //////////////////////////////////////////////////////////////////
+  //
   template <typename T>
   bool
   operator==(const std::complex<T>& Z, const type_identity_t<T>& real) noexcept
@@ -42,5 +61,62 @@ namespace LinearAlgebra
   operator!=(const type_identity_t<T>& real, const std::complex<T>& Z) noexcept
   {
     return not(Z == real);
+  }
+
+  // Arithmetic operators
+  //////////////////////////////////////////////////////////////////
+  //
+  template <typename T>
+  auto operator*(const std::complex<T>& c, const type_identity_t<T>& s)
+  {
+    return c * s;
+  }
+
+  template <typename T>
+  auto operator*(const type_identity_t<T>& s, const std::complex<T>& c)
+  {
+    return s * c;
+  }
+
+  template <typename T>
+  auto
+  operator/(const std::complex<T>& c, const type_identity_t<T>& s)
+  {
+    return c / s;
+  }
+
+  template <typename T>
+  auto
+  operator/(const type_identity_t<T>& s, const std::complex<T>& c)
+  {
+    return s / c;
+  }
+
+  template <typename T>
+  auto
+  operator+(const std::complex<T>& c, const type_identity_t<T>& s)
+  {
+    return c + s;
+  }
+
+  template <typename T>
+  auto
+  operator+(const type_identity_t<T>& s, const std::complex<T>& c)
+  {
+    return s + c;
+  }
+
+  template <typename T>
+  auto
+  operator-(const std::complex<T>& c, const type_identity_t<T>& s)
+  {
+    return c - s;
+  }
+
+  template <typename T>
+  auto
+  operator-(const type_identity_t<T>& s, const std::complex<T>& c)
+  {
+    return s - c;
   }
 }

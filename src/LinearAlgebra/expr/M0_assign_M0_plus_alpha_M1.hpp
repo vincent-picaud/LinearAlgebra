82a78c01252e8eca9e136c23152ba627c6d76ec7
picaud <picaud@am-080027980deb.intern>
2020-04-04 10:18:52 +0200

    code cleaning

--- src/LinearAlgebra/expr/M0_assign_M0_plus_alpha_M1.hpp
+++ src/LinearAlgebra/expr/M0_assign_M0_plus_alpha_M1.hpp
@@ -6,8 +6,5 @@
 
 #pragma once
 
+#include "LinearAlgebra/dense/matrix_header.hpp"
 #include "LinearAlgebra/expr/X0_assign_X0_plus_alpha_X1.hpp"
-
-#include "LinearAlgebra/dense/matrix_is_same.hpp"
-#include "LinearAlgebra/dense/matrix_transform.hpp"
-
--- src/LinearAlgebra/expr/V0_assign_V0_plus_alpha_V1.hpp
+++ src/LinearAlgebra/expr/V0_assign_V0_plus_alpha_V1.hpp
@@ -6,8 +6,5 @@
 
 #pragma once
 
+#include "LinearAlgebra/dense/vector_header.hpp"
 #include "LinearAlgebra/expr/X0_assign_X0_plus_alpha_X1.hpp"
-
-#include "LinearAlgebra/dense/vector_is_same.hpp"
-#include "LinearAlgebra/dense/vector_transform.hpp"
-

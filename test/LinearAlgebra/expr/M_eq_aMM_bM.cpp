6f21565d3061d91d62ebd19ef79d1ea76089da12
picaud <picaud@am-080027980deb.intern>
2020-04-07 11:09:30 +0200

    add expression M=αMM+βM

---
+++ test/LinearAlgebra/expr/M_eq_aMM_bM.cpp
@@ -0,0 +1,14 @@
+#include "LinearAlgebra/expr/M_eq_aMM_bM.hpp"
+
+// #include "LinearAlgebra/expr/dimension.hpp"
+// #include "LinearAlgebra/dense/matrix.hpp"
+// #include "LinearAlgebra/dense/vector.hpp"
+
+#include <gtest/gtest.h>
+
+using namespace LinearAlgebra;
+
+TEST(M_aMM_bM, alias)
+{
+
+}

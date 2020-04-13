3f7348f9128ca5a871aa9fa63ef428a284c2f9c4
picaud <picaud@am-080027980deb.intern>
2020-04-13 16:36:15 +0200

    add lacpak test

---
+++ test/LinearAlgebra/lapack/cholesky.cpp
@@ -0,0 +1,35 @@
+#include "LinearAlgebra/lapack/lapack.hpp"
+
+#include "LinearAlgebra/matrix.hpp"
+
+#include "LinearAlgebra/scalar_functions/dist_2_vector.hpp"
+
+#include <gtest/gtest.h>
+
+using namespace LinearAlgebra;
+
+TEST(Lapack, potrf)
+{
+  const size_t n = 5;
+
+  Vector<double> v(n), w(n);
+  v = 1;
+  Symmetric_Matrix<double> M(n, n);
+
+  M                                     = 0;
+  create_vector_view_matrix_diagonal(M) = 10;
+  M(4, 0)                               = 5;
+
+  w = M * v;
+
+  // L*LT
+  int info = Lapack::potrf(Lapack::Lapack_Order_Enum::Column_Major, Lapack::Lapack_UpLo_Enum::Low,
+                           M.I_size(), M.data(), M.leading_dimension());
+
+  auto L = create_matrix_view_lower_triangular(M.as_const());
+
+  w = inverse(L) * w;
+  w = inverse(transpose(L)) * w;
+
+  EXPECT_DOUBLE_EQ(1 + dist_2(v, w), 1);
+}

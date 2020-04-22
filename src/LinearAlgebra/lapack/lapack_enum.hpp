bacbe90b90919e62a5ef40495d0921002e78cedf
picaud <picaud@am-080027980deb.intern>
2020-04-22 23:21:21 +0200

    add doc

--- src/LinearAlgebra/lapack/lapack_enum.hpp
+++ src/LinearAlgebra/lapack/lapack_enum.hpp
@@ -10,12 +10,18 @@
 // library... A better place is maybe "lapack_config.hpp"
 namespace LinearAlgebra::Lapack
 {
+
+  // Note: we generally use enum CLASS, however here we already are
+  // into 2 nested namespaces (LinearAlgebra & Lapack)... this is
+  // certainly sufficient to avoid name collision.
+  //
   enum Lapack_Order_Enum : int
   {
     Row_Major    = LAPACK_ROW_MAJOR,
     Column_Major = LAPACK_COL_MAJOR
   };
 
+  //
   enum Lapack_UpLo_Enum : char
   {
     Up  = 'U',

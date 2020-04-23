4de1b86984019ffc71f0d808a9cad3aebfda3c07
picaud <picaud@am-080027980deb.intern>
2020-04-23 20:53:01 +0200

    posv test

---
+++ src/LinearAlgebra/lapack/subroutines_dense.hpp
@@ -0,0 +1,105 @@
+//
+// Wraps "subroutines" with dense (vector/matrix) objects
+//
+#pragma once
+
+// Note: this a quite "abstract" header, hence we include "lapack.hpp"
+// and dense vector & matrix headers
+//
+#include "LinearAlgebra/lapack/lapack.hpp"
+
+#include "LinearAlgebra/dense/matrix.hpp"
+#include "LinearAlgebra/dense/vector.hpp"
+
+#include "LinearAlgebra/utils/sfinae_vmt_helpers.hpp"
+
+namespace LinearAlgebra::Lapack
+{
+  template <typename MA_IMPL, typename MB_IMPL>
+  auto
+  posv(Dense_Matrix_Crtp<MA_IMPL>& A, Dense_Matrix_Crtp<MB_IMPL>& B) -> std::enable_if_t<
+      //
+      // CAVEAT: check if we can use potentially use an incomplete
+      //         type Lapack::To_Lapack_UpLo_v<MA_IMPL> instead of
+      //         Lapack::Lapack_UpLo_Enum::Low in a SFINEA context.
+      //
+      Always_True_v<decltype(Lapack::posv(Lapack::Lapack_Order_Enum::Column_Major,  //
+                                          Lapack::Lapack_UpLo_Enum::Low,            // <- here
+                                          A.I_size(),                               //
+                                          B.J_size(),                               //
+                                          A.data(),                                 //
+                                          A.leading_dimension(),                    //
+                                          B.data(),                                 //
+                                          B.leading_dimension()))>                  //
+                                                                                    //
+          and
+          //
+          (Is_Symmetric_Matrix_v<MA_IMPL> or Is_Hermitian_Matrix_v<MA_IMPL>)
+          //
+          and
+          //
+          Is_Full_Matrix_v<MB_IMPL>,
+      //
+      lapack_int>
+  {
+    return Lapack::posv(Lapack::Lapack_Order_Enum::Column_Major,  //
+                        Lapack::To_Lapack_UpLo_v<MA_IMPL>,        //
+                        A.I_size(),                               //
+                        B.J_size(),                               //
+                        A.data(),                                 //
+                        A.leading_dimension(),                    //
+                        B.data(),                                 //
+                        B.leading_dimension());                   //
+  }
+
+  template <typename MA_IMPL, typename VB_IMPL>
+  auto
+  posv(Dense_Matrix_Crtp<MA_IMPL>& A, Dense_Vector_Crtp<VB_IMPL>& B) -> std::enable_if_t<
+      //
+      // CAVEAT: check if we can use potentially use an incomplete
+      //         type Lapack::To_Lapack_UpLo_v<MA_IMPL> instead of
+      //         Lapack::Lapack_UpLo_Enum::Low in a SFINEA context.
+      //
+      Always_True_v<decltype(Lapack::posv(Lapack::Lapack_Order_Enum::Column_Major,  //
+                                          Lapack::Lapack_UpLo_Enum::Low,            // <- here
+                                          A.I_size(),                               //
+                                          1,                                        //
+                                          A.data(),                                 //
+                                          A.leading_dimension(),                    //
+                                          B.data(),                                 //
+                                          A.I_size()))>                             //
+                                                                                    //
+          and
+          //
+          (Is_Symmetric_Matrix_v<MA_IMPL> or Is_Hermitian_Matrix_v<MA_IMPL>),
+      //
+      lapack_int>
+  {
+    if (B.increment() == 1)
+    {
+      return Lapack::posv(Lapack::Lapack_Order_Enum::Column_Major,  //
+                          Lapack::To_Lapack_UpLo_v<MA_IMPL>,        //
+                          A.I_size(),                               //
+                          1,                                        //
+                          A.data(),                                 //
+                          A.leading_dimension(),                    //
+                          B.data(),                                 //
+                          A.I_size());                              //
+                                                                    //
+    }
+
+    // Must copy B to have
+    auto B_cpy = create_default_storable(B);
+    B_cpy      = B;
+    assert(B_cpy.increment() == 1);
+
+    return Lapack::posv(Lapack::Lapack_Order_Enum::Column_Major,  //
+                        Lapack::To_Lapack_UpLo_v<MA_IMPL>,        //
+                        A.I_size(),                               //
+                        1,                                        //
+                        A.data(),                                 //
+                        A.leading_dimension(),                    //
+                        B_cpy.data(),                             //
+                        A.I_size());                              //
+  }
+}
---
+++ test/LinearAlgebra/lapack/subroutines_dense.cpp
@@ -0,0 +1,29 @@
+#include "LinearAlgebra/lapack/subroutines_dense.hpp"
+
+#include "LinearAlgebra/dense/matrix_fill.hpp"
+
+#include "LinearAlgebra/matrix.hpp"
+#include "LinearAlgebra/vector.hpp"
+
+#include <gtest/gtest.h>
+
+using namespace LinearAlgebra;
+
+TEST(Subroutines_Dense, posv)
+{
+  using T        = double;
+  const size_t n = 5;
+  Symmetric_Matrix<T> H(n, n);
+  Vector<T> q(n);
+
+  fill_indexed([](auto i, auto j) { return 1 / T(i + j + 1); }, H);
+  q = -1;
+
+  Lapack::posv(H, q);
+
+  fill_indexed([](auto i, auto j) { return 1 / T(i + j + 1); }, H);
+  Vector<T> q_dup(n);
+  q_dup = H * q;
+
+  scan([](const auto qi) { EXPECT_FLOAT_EQ(qi, -1); }, q_dup);
+}

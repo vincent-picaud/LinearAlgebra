ac9a95db0e407eb65c7355a71ecc7f0cd63b380e
picaud <picaud@am-080027980deb.intern>
2020-04-23 17:22:05 +0200

    (partial) add posv

--- src/LinearAlgebra/lapack/subroutines.hpp
+++ src/LinearAlgebra/lapack/subroutines.hpp
@@ -6,6 +6,8 @@
 #error
 #endif
 
+#include "LinearAlgebra/lapack/lapack_enum.hpp"
+
 #include <ccomplex>
 #include <cstddef>
 
@@ -56,35 +58,78 @@ namespace LinearAlgebra::Lapack
 
   // potrs solve linear system from portf result
   //
-  // CAVEAT: if rhs is a vector, its inc must be =1 
+  // CAVEAT: if rhs is a vector, its increment must be = 1 (otherwise
+  //         a copy is mandatory)
+  //
   //================================================================
   //
 
   lapack_int
-  potrf(const Lapack_Order_Enum matrix_layout, const Lapack_UpLo_Enum uplo, const std::size_t n,
+  potrs(const Lapack_Order_Enum matrix_layout, const Lapack_UpLo_Enum uplo, const std::size_t n,
         const std::size_t nrhs, float* a, const std::size_t lda, float* b, const std::size_t ldb)
   {
     return LAPACKE_spotrs(matrix_layout, uplo, n, nrhs, a, lda, b, ldb);
   };
   lapack_int
-  potrf(const Lapack_Order_Enum matrix_layout, const Lapack_UpLo_Enum uplo, const std::size_t n,
+  potrs(const Lapack_Order_Enum matrix_layout, const Lapack_UpLo_Enum uplo, const std::size_t n,
         const std::size_t nrhs, double* a, const std::size_t lda, double* b, const std::size_t ldb)
   {
     return LAPACKE_dpotrs(matrix_layout, uplo, n, nrhs, a, lda, b, ldb);
   };
 
   lapack_int
-  potrf(const Lapack_Order_Enum matrix_layout, const Lapack_UpLo_Enum uplo, const std::size_t n,
-        const std::size_t nrhs, std::complex<float>* a, const std::size_t lda, std::complex<float>* b, const std::size_t ldb)
+  potrs(const Lapack_Order_Enum matrix_layout, const Lapack_UpLo_Enum uplo, const std::size_t n,
+        const std::size_t nrhs, std::complex<float>* a, const std::size_t lda,
+        std::complex<float>* b, const std::size_t ldb)
   {
     return LAPACKE_cpotrs(matrix_layout, uplo, n, nrhs, a, lda, b, ldb);
   };
 
   lapack_int
-  potrf(const Lapack_Order_Enum matrix_layout, const Lapack_UpLo_Enum uplo, const std::size_t n,
-        const std::size_t nrhs, std::complex<double>* a, const std::size_t lda, std::complex<double>* b, const std::size_t ldb)
+  potrs(const Lapack_Order_Enum matrix_layout, const Lapack_UpLo_Enum uplo, const std::size_t n,
+        const std::size_t nrhs, std::complex<double>* a, const std::size_t lda,
+        std::complex<double>* b, const std::size_t ldb)
   {
     return LAPACKE_zpotrs(matrix_layout, uplo, n, nrhs, a, lda, b, ldb);
   };
 
+  //
+  // DRIVER routines
+  //
+  // posv solves linear system (compared to potrs also perform
+  // iterative refinement etc...)
+  //
+  // CAVEAT: if rhs is a vector, its increment must be = 1 (otherwise
+  //         a copy is mandatory)
+  //
+  //================================================================
+  //
+  lapack_int
+  posv(const Lapack_Order_Enum matrix_layout, const Lapack_UpLo_Enum uplo, const std::size_t n,
+       const std::size_t nrhs, float* a, const std::size_t lda, float* b, const std::size_t ldb)
+  {
+    return LAPACKE_sposv(matrix_layout, uplo, n, nrhs, a, lda, b, ldb);
+  };
+  lapack_int
+  posv(const Lapack_Order_Enum matrix_layout, const Lapack_UpLo_Enum uplo, const std::size_t n,
+       const std::size_t nrhs, double* a, const std::size_t lda, double* b, const std::size_t ldb)
+  {
+    return LAPACKE_dposv(matrix_layout, uplo, n, nrhs, a, lda, b, ldb);
+  };
+
+  lapack_int
+  posv(const Lapack_Order_Enum matrix_layout, const Lapack_UpLo_Enum uplo, const std::size_t n,
+       const std::size_t nrhs, std::complex<float>* a, const std::size_t lda,
+       std::complex<float>* b, const std::size_t ldb)
+  {
+    return LAPACKE_cposv(matrix_layout, uplo, n, nrhs, a, lda, b, ldb);
+  };
+
+  lapack_int
+  posv(const Lapack_Order_Enum matrix_layout, const Lapack_UpLo_Enum uplo, const std::size_t n,
+       const std::size_t nrhs, std::complex<double>* a, const std::size_t lda,
+       std::complex<double>* b, const std::size_t ldb)
+  {
+    return LAPACKE_zposv(matrix_layout, uplo, n, nrhs, a, lda, b, ldb);
+  };
 }
---
+++ test/LinearAlgebra/lapack/posv.cpp
@@ -0,0 +1,160 @@
+#include "LinearAlgebra/dense/matrix_crtp.hpp"
+#include "LinearAlgebra/dense/matrix_fwd.hpp"
+#include "LinearAlgebra/lapack/lapack_enum.hpp"
+#include "LinearAlgebra/lapack/subroutines.hpp"
+
+#include "LinearAlgebra/dense/matrix.hpp"
+#include "LinearAlgebra/dense/vector.hpp"
+#include "LinearAlgebra/utils/always.hpp"
+#include "LinearAlgebra/utils/crtp.hpp"
+
+#include <gtest/gtest.h>
+#include <type_traits>
+
+namespace LinearAlgebra
+{
+  // template <typename IMPL>
+  // constexpr std::false_type
+  // is_symmetric_matrix_p(const Crtp<IMPL>&) noexcept
+  // {
+  //   return {};
+  // }
+
+  // template <typename IMPL>
+  // constexpr std::integral_constant<bool, (IMPL::matrix_special_structure_type::value ==
+  //                                         Matrix_Special_Structure_Enum::Symmetric)>
+  // is_symmetric_matrix_p(const Dense_Matrix_Crtp<IMPL>&) noexcept
+  // {
+  //   return {};
+  // }
+  // template <typename IMPL>
+  // constexpr std::integral_constant<bool, (IMPL::matrix_special_structure_type::value ==
+  //                                         Matrix_Special_Structure_Enum::Hermitian)>
+  // is_hermitian_matrix_p(const Dense_Matrix_Crtp<IMPL>&) noexcept
+  // {
+  //   return {};
+  // }
+  // template <typename IMPL>
+  // constexpr std::integral_constant<bool, (IMPL::matrix_special_structure_type::value ==
+  //                                         Matrix_Special_Structure_Enum::Symmetric) or
+  //                                            (IMPL::matrix_special_structure_type::value ==
+  //                                             Matrix_Special_Structure_Enum::Hermitian)>
+  // is_symmetric_or_hermitian_matrix_p(const Dense_Matrix_Crtp<IMPL>&) noexcept
+  // {
+  //   return {};
+  // }
+  // template <typename IMPL>
+  // constexpr std::integral_constant<bool, (IMPL::matrix_special_structure_type::value ==
+  //                                         Matrix_Special_Structure_Enum::None)>
+  // is_full_matrix_p(const Dense_Matrix_Crtp<IMPL>&) noexcept
+  // {
+  //   return {};
+  // }
+
+  // template <typename T>
+  // struct Is_Symmetric_Matrix : decltype(is_symmetric_matrix_p(std::declval<T>()))
+  // {
+  // };
+
+  // template <typename T>
+  // constexpr bool Is_Symmetric_Matrix_v = Is_Symmetric_Matrix<T>::value;
+
+  // template <typename T>
+  // struct Is_Full_Matrix : decltype(is_full_matrix_p(std::declval<T>()))
+  // {
+  // };
+
+  // template <typename T>
+  // constexpr bool Is_Full_Matrix_v = Is_Full_Matrix<T>::value;
+
+  template <typename T, typename ENABLE = void>
+  struct Is_Symmetric_Matrix : std::false_type
+  {
+  };
+
+  template <typename T>
+  struct Is_Symmetric_Matrix<T, std::enable_if_t<Is_Crtp_Interface_Of_v<Dense_Matrix_Crtp, T> and
+                                                 (T::matrix_special_structure_type::value ==
+                                                  Matrix_Special_Structure_Enum::Symmetric)>>
+      : std::true_type
+  {
+  };
+
+  template <typename T>
+  constexpr bool Is_Symmetric_Matrix_v = Is_Symmetric_Matrix<T>::value;
+
+  template <typename T, typename ENABLE = void>
+  struct Is_Full_Matrix : std::false_type
+  {
+  };
+
+  template <typename T>
+  struct Is_Full_Matrix<T, std::enable_if_t<Is_Crtp_Interface_Of_v<Dense_Matrix_Crtp, T> and
+                                            (T::matrix_special_structure_type::value ==
+                                             Matrix_Special_Structure_Enum::None)>> : std::true_type
+  {
+  };
+
+  template <typename T>
+  constexpr bool Is_Full_Matrix_v = Is_Full_Matrix<T>::value;
+
+  //================================================================
+
+  // template <Matrix_Storage_Mask_Enum OP_M>
+  //   struct To_Lapack_UpLo;
+  
+  // template<typename IMPL>
+  // constexpr std::integral_constant<Lapack::Lapack_UpLo_Enum,Lapack::Lapack_UpLo_Enum::Low>
+  // to_lapack_uplo(const std::integral_constant<)
+  // {
+  // }
+
+  //================================================================
+
+  template <typename MA_IMPL, typename MB_IMPL>
+  auto
+  posv(Dense_Matrix_Crtp<MA_IMPL>& A, Dense_Matrix_Crtp<MB_IMPL>& B) -> std::enable_if_t<
+      Always_True_v<decltype(Lapack::posv(
+          Lapack::Lapack_Order_Enum::Column_Major, Lapack::Lapack_UpLo_Enum::Low, A.I_size(),
+          B.J_size(), A.data(), A.leading_dimension(), B.data(), B.leading_dimension()))> and
+          Is_Symmetric_Matrix_v<MA_IMPL> and Is_Full_Matrix_v<MB_IMPL>,
+      lapack_int>
+  {
+    return Lapack::posv(Lapack::Lapack_Order_Enum::Column_Major, Lapack::Lapack_UpLo_Enum::Low,
+                        A.I_size(), B.J_size(), A.data(), A.leading_dimension(), B.data(),
+                        B.leading_dimension());
+  }
+
+  // template <typename MA_IMPL, typename MB_IMPL>
+  // auto
+  // posv(Dense_Matrix_Crtp<MA_IMPL>& A, Dense_Matrix_Crtp<MB_IMPL>& B) -> std::enable_if_t<
+  //     Always_True_v<decltype(Lapack::posv(
+  //         Lapack::Lapack_Order_Enum::Column_Major, Lapack::Lapack_UpLo_Enum::Low, A.I_size(),
+  //         B.J_size(), A.data(), A.leading_dimension(), B.data(),
+  //         B.leading_dimension()))>and decltype(is_symmetric_or_hermitian_matrix_p(A))::value,
+  //     lapack_int>
+  // {
+  //   return Lapack::posv(Lapack::Lapack_Order_Enum::Column_Major, Lapack::Lapack_UpLo_Enum::Low,
+  //                       A.I_size(), B.J_size(), A.data(), A.leading_dimension(), B.data(),
+  //                       B.leading_dimension());
+  // }
+
+  template <typename T>
+  struct Is_Symmetric_Matrix<T, std::enable_if_t<Is_Crtp_Interface_Of_v<Dense_Vector_Crtp, T>>>
+      : std::true_type
+  {
+  };
+}
+
+using namespace LinearAlgebra;
+using namespace LinearAlgebra::Lapack;
+
+TEST(Posv, sfinae)
+{
+  Matrix<double> A(5, 5);
+  Tiny_Symmetric_Matrix<double, 5> B;
+
+  posv(B, A);
+
+  EXPECT_TRUE(Is_Symmetric_Matrix_v<Vector<double>>);
+}

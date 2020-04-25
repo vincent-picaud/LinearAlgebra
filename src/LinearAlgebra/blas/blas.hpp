271dbc7cf071064f1130a615e79b97dcbe45320c
picaud <picaud@am-080027980deb.intern>
2020-04-25 12:23:34 +0200

    add blas::subroutine_dense

--- src/LinearAlgebra/blas/blas.hpp
+++ src/LinearAlgebra/blas/blas.hpp
@@ -7,6 +7,7 @@
 #if (HAS_BLAS)
 #include "LinearAlgebra/blas/is_cblas_supported_scalar.hpp"
 #include "LinearAlgebra/blas/subroutines.hpp"
+#include "LinearAlgebra/blas/subroutines_dense.hpp"
 #include "LinearAlgebra/blas/to_cblas_diag.hpp"
 #include "LinearAlgebra/blas/to_cblas_transpose.hpp"
 #include "LinearAlgebra/blas/to_cblas_uplo.hpp"
---
+++ src/LinearAlgebra/blas/subroutines_dense.hpp
@@ -0,0 +1,51 @@
+//
+// Wraps "subroutines" with dense (vector/matrix) objects
+//
+#pragma once
+
+// Note: this a quite "abstract" header, hence we include "blas.hpp"
+// and dense vector & matrix headers
+//
+#include "LinearAlgebra/blas/blas.hpp"
+
+#include "LinearAlgebra/dense/matrix.hpp"
+#include "LinearAlgebra/dense/vector.hpp"
+
+#include "LinearAlgebra/dense/vector_crtp.hpp"
+#include "LinearAlgebra/utils/always.hpp"
+#include "LinearAlgebra/utils/element_type.hpp"
+#include "LinearAlgebra/utils/sfinae_vmt_helpers.hpp"
+
+namespace LinearAlgebra::Blas
+{
+  /////////////
+  // Level 1 //
+  /////////////
+  //
+  //                            dim scalar vector   vector   scalars              5-element prefixes
+  //
+  // - [ ] : SUBROUTINE _ROTG (                                      A, B, C, S )          S, D
+  // - [ ] : SUBROUTINE _ROTMG(                              D1, D2, A, B,        PARAM )  S, D
+  // - [ ] : SUBROUTINE _ROT  ( N,         X, INCX, Y, INCY,               C, S )          S, D
+  // - [ ] : SUBROUTINE _ROTM ( N,         X, INCX, Y, INCY,                      PARAM )  S, D
+  // - [ ] : SUBROUTINE _SWAP ( N,         X, INCX, Y, INCY )                              S, D, C, Z
+  // - [X] : SUBROUTINE _SCAL ( N,  ALPHA, X, INCX )                                       S, D, C, Z, CS, ZD
+  // - [ ] : SUBROUTINE _COPY ( N,         X, INCX, Y, INCY )                              S, D, C, Z
+  // - [ ] : SUBROUTINE _AXPY ( N,  ALPHA, X, INCX, Y, INCY )                              S, D, C, Z
+  // - [ ] : FUNCTION   _DOT  ( N,         X, INCX, Y, INCY )                              S, D, DS
+  // - [ ] : FUNCTION   _DOTU ( N,         X, INCX, Y, INCY )                              C, Z
+  // - [ ] : FUNCTION   _DOTC ( N,         X, INCX, Y, INCY )                              C, Z
+  // - [ ] : FUNCTION   __DOT ( N,  ALPHA, X, INCX, Y, INCY )                              SDS
+  // - [ ] : FUNCTION   _NRM2 ( N,         X, INCX )                                       S, D, SC, DZ
+  // - [ ] : FUNCTION   _ASUM ( N,         X, INCX )                                       S, D, SC, DZ
+  // - [ ] : FUNCTION   I_AMAX( N,         X, INCX )                                       S, D, C, Z
+
+  template <typename V_IMPL>
+  auto
+  scal(const Element_Type_t<V_IMPL>& alpha, Dense_Vector_Crtp<V_IMPL>& V) -> std::enable_if_t<
+      Always_True_v<decltype(Blas::scal(V.size(), alpha, V.data(), V.increment()))>>
+  {
+    Blas::scal(V.size(), alpha, V.data(), V.increment());
+  }
+
+}  // namespace LinearAlgebra::Blas

01eb6e4d43579130d2677baa12a0ff5b3d76c899
picaud <picaud@am-080027980deb.intern>
2020-04-26 22:17:22 +0200

    add base for syrk test

---
+++ test/LinearAlgebra/metaexpr/syrk.cpp
@@ -0,0 +1,59 @@
+
+#include "LinearAlgebra/dense/matrix_fwd.hpp"
+#include "LinearAlgebra/matrix.hpp"
+#include "LinearAlgebra/vector.hpp"
+
+#include <gtest/gtest.h>
+
+using namespace LinearAlgebra;
+
+TEST(Syrk, Gemm)
+{
+  Matrix<double> A(2, 3);
+
+  A(0, 0) = 0;
+  A(1, 0) = 1;
+  A(0, 1) = -4;
+  A(1, 1) = 3;
+  A(0, 2) = 2;
+  A(1, 2) = -4;
+
+  // Precomputed AtA & AAt
+  //
+  Matrix<double> AAt(2, 2);
+
+  AAt(0, 0) = 20;
+  AAt(1, 0) = -20;
+
+  AAt(0, 1) = -20;
+  AAt(1, 1) = 26;
+
+  Matrix<double> AtA(3, 3);
+
+  AtA(0, 0) = 1;
+  AtA(1, 0) = 3;
+  AtA(2, 0) = -4;
+
+  AtA(0, 1) = 3;
+  AtA(1, 1) = 25;
+  AtA(2, 1) = -20;
+
+  AtA(0, 2) = -4;
+  AtA(1, 2) = -20;
+  AtA(2, 2) = 20;
+
+  Symmetric_Matrix<double> C_AAt(2, 2), C_AtA(3, 3);
+  C_AAt = 1;
+  C_AtA = 1;
+
+  C_AAt = 2 * identity(A) * transpose(A) + 3 * C_AAt;
+  C_AtA = 2 * transpose(A) * identity(A) + 3 * C_AtA;
+
+  Matrix<double> Full_C_AAt(2, 2), Full_C_AtA(3, 3);
+
+  Full_C_AAt = 1;
+  Full_C_AtA = 1;
+
+  Full_C_AAt = 2 * identity(A) * transpose(A) + 3 * Full_C_AAt;
+  Full_C_AtA = 2 * transpose(A) * identity(A) + 3 * Full_C_AtA;
+}

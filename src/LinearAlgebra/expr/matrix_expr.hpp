189875cc9235e1d8936f49e7c3dfe29c190c421e
picaud <picaud@am-080027980deb.intern>
2020-05-02 11:51:08 +0200

    include fix

--- src/LinearAlgebra/expr/matrix_expr.hpp
+++ src/LinearAlgebra/expr/matrix_expr.hpp
@@ -2,9 +2,9 @@
 //
 #pragma once
 
-#include "LinearAlgebra/expr/expr.hpp"
-
+#include "LinearAlgebra/expr/M_eq_aMM_bM_matrix.hpp"
 #include "LinearAlgebra/expr/X_eq_aX_matrix.hpp"
+#include "LinearAlgebra/expr/expr.hpp"
 
 #include "LinearAlgebra/expr/V0_assign_alpha_op_M_V1_plus_beta_VX_header.hpp"
 #include "LinearAlgebra/expr/V0_assign_alpha_op_M_VX_header.hpp"

75d14233f91f79669e205f5bc81a22d2af60c41c
Picaud Vincent <picaud.vincent@gmail.com>
2020-06-25 16:16:27 +0200

    test example doc

--- examples/matrix_view.cpp
+++ examples/matrix_view.cpp
@@ -1,3 +1,4 @@
+// [BEGIN_matrix_view]
 #include "LinearAlgebra/matrix.hpp"
 
 using namespace LinearAlgebra;
@@ -5,31 +6,42 @@ using namespace LinearAlgebra;
 int
 main()
 {
+  // [DOC]
+  //
+  // Declares a *static* matrix of dimension $5\times 6$.
   Tiny_Matrix<int, 5, 6> mat;
-
   mat = 1;
 
+  // [DOC]
+  //
+  // Create a sub-matrix [1:3,2:5] view and fills it with 1
   auto view_mat = create_matrix_view(mat, 1, 3, 2, 5);
+  view_mat      = -1;
 
-  view_mat = -1;
+  std::cout << "full matrix 1:" << mat << std::endl;
 
-  std::cout << "full matrix:" << mat << std::endl;
-
-  //================
-
+  // [DOC]
+  //
+  // A static, symmetric matrix of dimension $6\times 6$.
   Tiny_Symmetric_Matrix<int, 6> mat_S;
 
+  // [DOC]
+  //
+  // Creates a *full* view and fills it with 0
   create_matrix_view_full(mat_S) = 0;  // fill the full matrix
 
-  mat_S = 1;  // here as mat_S is symmetric
-              // only fill lower triangular part is filled
+  // [DOC]
+  //
+  // Here as =mat_S= is symmetric
+  // only fills the lower triangular part...
+  //
+  mat_S = 1;
 
-  // Take a subview of a _symmetric matrix_
+  // Take a *symmetric* sub-matrix [1:3,1:3] view and fills it with -1
   auto view_mat_S = create_matrix_view(mat_S, 1, 3, 1, 3);
+  view_mat_S      = -1;
 
-  view_mat_S = -1;  // here the subview mat_S is symmetric too and
-                    // only the lower triangular part will be filled
-
-  std::cout << "full matrix:" << create_matrix_view_full(mat_S) << std::endl;
-  std::cout << "submatrix:" << mat_S << std::endl;
+  std::cout << "full matrix 2:" << create_matrix_view_full(mat_S) << std::endl;
+  std::cout << "submatrix   3:" << mat_S << std::endl;
 }
+// [END_matrix_view]

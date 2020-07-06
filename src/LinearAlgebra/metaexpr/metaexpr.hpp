38eae8bd6228a41a5a3cce0fd4fe137d524bcda1
Picaud Vincent <picaud.vincent@gmail.com>
2020-07-06 12:45:22 +0200

    doc

--- src/LinearAlgebra/metaexpr/metaexpr.hpp
+++ src/LinearAlgebra/metaexpr/metaexpr.hpp
@@ -1,7 +1,10 @@
-// Main metaexpr header
-//
+// [[file:metaexpr.org]]
+// [BEGIN_metaexpr_hpp]
 #pragma once
-
+// [DOC]
+//
+// [[file:call_assign_from_metaexpr.org][call_assign_from_metaexpr.hpp]]
 #include "call_assign_from_metaexpr.hpp"
 #include "metaexpr_crtp.hpp"
 #include "operator_overloading.hpp"
+// [END_metaexpr_hpp]
